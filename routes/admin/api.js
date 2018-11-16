0af9a43ee616992a2463e30836a9d825f62f4e76
Alan <alan@kh1650>
2018-11-16 11:17:26 +0800

    claim the share variable at the begining

--- routes/admin/api.js
+++ routes/admin/api.js
@@ -1,14 +1,16 @@
 "use strict";
 
+const log4js = require('alog4js');
+const logLevel = ['trace','debug','info','warn','error','fatal'];
+
 exports.log = function(req, res){
-    const logger = require('alog4js').getLogger('routes.admin.log');
+    const logger = log4js.getLogger('routes.admin.log');
     new Promise(function(resolve, reject){
-        const logger = require('alog4js').getLogger();
         logger.info("req.query", req.query)
-        const logLevel = ['trace','debug','info','warn','error','fatal'];
         let level = (req.query.level) ? String(req.query.level) : undefined;
         if(logLevel.includes(level)){
             logger.level = level;
+            log4js.update('*', level);//apply log level to all related domain
             return resolve("log level set "+level)
         }else{
             return reject({errorCode:400, msg:"Bad Parameter"})
@@ -21,12 +23,9 @@ exports.log = function(req, res){
 };
 
 exports.logUpdate = function(req, res){
-    const logger = require('alog4js').getLogger('routes.admin.logUpdate');
+    const logger = log4js.getLogger('routes.admin.logUpdate');
     new Promise(function(resolve, reject){
-        const log4js = require('alog4js');
-        const logger = log4js.getLogger();
         logger.info("req.query", req.query)
-        const logLevel = ['trace','debug','info','warn','error','fatal'];
         let level = (req.query.level) ? String(req.query.level) : undefined;
         let show = (req.query.show) ? String(req.query.show) : undefined;
         if(logLevel.includes(level) && show){
